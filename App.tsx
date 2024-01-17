4ed33bd7bee5a1ad575999f97ad21b3ff61a5fd5
jonasbrk <jonas0378@gmail.com>
2024-01-17 20:37:36 -0300

    first implementation test

--- App.tsx
+++ App.tsx
@@ -5,39 +5,136 @@
  * @format
  */
 
-import React from 'react'
-import { SafeAreaView, Text, useColorScheme, View } from 'react-native'
+import { API_BASE_URL } from '@env'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Button, Text, View } from 'react-native'
+import AudioRecorderPlayer from 'react-native-audio-recorder-player'
 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+const audioRecorderPlayer = new AudioRecorderPlayer()
 
-function App(): React.JSX.Element {
-	const isDarkMode = useColorScheme() === 'dark'
+const App = () => {
+  const [isRecording, setIsRecording] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [recordUri, setRecordUri] = useState('')
+  const [transcribedTxt, setTranscribedTxt] = useState('')
+  const [loading, setIsLoading] = useState(false)
 
-	const backgroundStyle = {
-		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-	}
+  const startRecording = async () => {
+    setIsLoading(false)
+    try {
+      const result = await audioRecorderPlayer.startRecorder()
+      console.log(result)
+      setIsRecording(true)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-	return (
-		<SafeAreaView style={backgroundStyle}>
-			<View
-				style={{
-					width: '100%',
-					marginTop: 'auto',
-				}}
-			>
-				<Text
-					style={{
-						fontSize: 24,
-						width: '100%',
+  const stopRecording = async () => {
+    setIsLoading(false)
+    try {
+      const result = await audioRecorderPlayer.stopRecorder()
+      console.log(result)
+      setIsRecording(false)
+      setRecordUri(result)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-						textAlign: 'center',
-					}}
-				>
-          hellow world
-				</Text>
-			</View>
-		</SafeAreaView>
-	)
+  const startPlaying = async () => {
+    setIsLoading(false)
+    try {
+      const result = await audioRecorderPlayer.startPlayer()
+      console.log(result)
+      setIsPlaying(true)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const stopPlaying = async () => {
+    setIsLoading(false)
+    try {
+      await audioRecorderPlayer.stopPlayer()
+      setIsPlaying(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleTranscribing = async () => {
+    try {
+      if (isPlaying) {
+        await stopPlaying()
+      }
+
+      if (!recordUri) {
+        console.log('no record path')
+        setIsRecording(false)
+        setIsLoading(false)
+        return
+      }
+
+      setIsLoading(true)
+
+      const formData = new FormData()
+      formData.append('files', {
+        uri: recordUri,
+        type: 'audio/mp4',
+        name: 'audioFile',
+      })
+
+      console.log('arquivo', formData)
+      console.log(API_BASE_URL)
+
+      const res = await axios({
+        method: 'POST',
+        url: API_BASE_URL + '/transcribe',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      if (!res.data || !res.data.results.length) {
+        console.log('no transcription')
+        setIsRecording(false)
+        setIsLoading(false)
+        return
+      }
+
+      const transcription = res.data.results[0].transcript
+      console.log('result', transcription)
+
+      setTranscribedTxt(transcription)
+      setIsRecording(false)
+      setIsLoading(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <View>
+      <Button
+        title={isRecording ? 'Stop Recording' : 'Start Recording'}
+        onPress={isRecording ? stopRecording : startRecording}
+      />
+      <Button
+        title={isPlaying ? 'Stop Playing' : 'Start Playing'}
+        onPress={isPlaying ? stopPlaying : startPlaying}
+      />
+      <Button
+        title={loading ? 'Transcribing...' : 'Transcribe'}
+        disabled={loading}
+        onPress={handleTranscribing}
+      />
+
+      {transcribedTxt && <Text>{transcribedTxt}</Text>}
+    </View>
+  )
 }
 
 export default App
